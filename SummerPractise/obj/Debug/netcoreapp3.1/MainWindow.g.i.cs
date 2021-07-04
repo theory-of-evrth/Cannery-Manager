e1a4e1986686226728e57ce90c3492052b1f2dd9
Theory of everything <58880827+Theory-of-everything1@users.noreply.github.com>
2021-07-04 13:17:05 +0300

    first test edits

--- SummerPractise/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ SummerPractise/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -56,7 +56,7 @@ namespace SummerPractise {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SummerPractise;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SummerPractise;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
