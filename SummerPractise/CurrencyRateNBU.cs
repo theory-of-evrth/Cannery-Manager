bd37c19c1e91e07dcd0227c26d2988da3eddca29
Евгений Дёмин <feedpprple@gmail.com>
2021-07-05 19:28:03 +0300

    Add classes for parsing NBU json
    add class CurrencyRate
    add class Parser
    
    also
    add Newtonsoft.Json package

---
+++ SummerPractise/CurrencyRateNBU.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SummerPractise
+{
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public class CurrencyRateNBU
+    {
+        [JsonProperty(PropertyName ="r030")]
+        public int r030 { get; set; }
+
+        [JsonProperty(PropertyName ="txt")]
+        public string txt { get; set; }
+
+        [JsonProperty(PropertyName ="rate")]
+        public float rate { get; set; }
+
+        [JsonProperty(PropertyName ="cc")]
+        public string cc { get; set; } 
+
+        [JsonProperty(PropertyName = "exchangedate ")]
+        public string exchangedate { get; set; }
+
+        public string[] ToStringArray()
+        {
+            string[] arr = new string[] { r030.ToString(), txt, rate.ToString(), cc, exchangedate };
+            return arr;
+        }
+    }
+}
--- SummerPractise/Parser.cs
+++ SummerPractise/Parser.cs
@@ -1,11 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace SummerPractise
 {
-    class Parser
+    public class Parser
     {
+        private string jsonResponse;
+        private List<CurrencyRateNBU> currencyRatesNBU;
 
+        public void FillListFromJSONToCurrentDayNBU()
+        {
+            WebRequesterToAPI webRequesterToAPI = new WebRequesterToAPI();
+            webRequesterToAPI.MakeRequestToCurrentDay();
+            using (WebResponse webResponse = webRequesterToAPI.GetResponse())
+            {
+                using(Stream dataStream = webResponse.GetResponseStream())
+                {
+                    StreamReader streamReader = new StreamReader(dataStream);
+                    jsonResponse = streamReader.ReadToEnd();
+                }
+            }
+            currencyRatesNBU = JsonConvert.DeserializeObject<List<CurrencyRateNBU>>(jsonResponse);
+        }
+
+        public void FillListFromJSONToDayNBU(string date)
+        {
+            WebRequesterToAPI webRequesterToAPI = new WebRequesterToAPI();
+            webRequesterToAPI.MakeRequestToDate(date);
+            using (WebResponse webResponse = webRequesterToAPI.GetResponse())
+            {
+                using (Stream dataStream = webResponse.GetResponseStream())
+                {
+                    StreamReader streamReader = new StreamReader(dataStream);
+                    jsonResponse = streamReader.ReadToEnd();
+                }
+            }
+            currencyRatesNBU = JsonConvert.DeserializeObject<List<CurrencyRateNBU>>(jsonResponse);
+        }
+
+        public List<CurrencyRateNBU> GetCurrencyRatesNBU()
+        {
+            return currencyRatesNBU;
+        }
+        
     }
 }
