8ab0981731760d9743d60431766a7160ef3255fa
Theory of everything <58880827+Theory-of-everything1@users.noreply.github.com>
2021-07-06 18:51:37 +0300

    User Entity
    Some test data, user class, db connection and first manipulation (not ours though, it will be connected once all of them are done)

--- SummerPractise/MainWindow.xaml.cs
+++ SummerPractise/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿
+using SummerPractise.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,10 +26,41 @@ namespace SummerPractise
         public MainWindow()
         {
             InitializeComponent();
+
+            StorageContext db = new StorageContext();
+            
+            //    // создаем два объекта User
+            //User user1 = new User { id = 1, name = "Kolya", password = "Nikolay" };
+            //User user2 = new User { id = 2, name = "Sam", password = "1234567890" };
+            //User user1 = new User { id = 3, name = "Kolya", password = "Nikolay" };
+            //User user2 = new User { id = 4, name = "Sam", password = "1234567890" };
+            
+            //    // добавляем их в бд
+            //db.Users.Add(user1);
+            //db.Users.Add(user2);
+            //db.SaveChanges();
+
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users;
+                foreach (User u in users)
+                {
+                    if (u.id == 4)
+                    MessageBox.Show($"{u.id}.{u.name} - {u.password}");
+                }
+
         }
         private void Button_Login(Object sender, RoutedEventArgs e)
         {
+            string login = loginBox.Text;
+            string password = passwordBox.Password;
 
+            StorageContext db = new StorageContext();
+            var users = db.Users;
+            foreach (User u in users)
+            {
+                if (u.name == login && u.password == password)
+                    MessageBox.Show($"Успешный логин, человек с id {u.id}");
+            }
         }
     }
 }
---
+++ SummerPractise/Model/StorageContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerPractise.Model
+{
+    class StorageContext : DbContext
+    {
+        public StorageContext()
+            : base("DbConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
