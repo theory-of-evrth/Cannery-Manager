7c43db37eb4d429bd925db3f894f852124bdd1c5
Theory of everything <58880827+Theory-of-everything1@users.noreply.github.com>
2021-07-08 18:17:24 +0300

    MenuPage design

---
+++ SummerPractise/MenuPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SummerPractise
+{
+    /// <summary>
+    /// Interaction logic for MenuPage.xaml
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
